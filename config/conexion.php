102f99019e59c7e8c1b6e7ac57ea89ce2e80ce4d
Sally110303 <jayoloayzasallyyenny@gmail.com>
2024-11-22 16:09:30 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,9 +1,12 @@
 <?php
+require_once 'datos.php';
+
 $servidor = "mysql:dbname=" . DB . ";host=" . SERVIDOR;
 
 try {
-    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    //echo "<script>alert('Conectado...');</script>";
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // echo "<script>alert('Error: " . $e->getMessage() . "');</script>";
+    die("Error en la conexión a la base de datos: " . $e->getMessage());
 }
+?>
